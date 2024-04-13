dea3a86d6035398b272a742138addf58a7a7579e
Mohamed Ahmed <98103696+MohamedAhmed142000@users.noreply.github.com>
2024-04-13 19:35:20 +0200

    add three function to calculate hr ,hw and (hr+hw) percentage

--- app/src/main/java/com/example/ipcd/data/AnswerStatistics.kt
+++ app/src/main/java/com/example/ipcd/data/AnswerStatistics.kt
@@ -9,32 +9,31 @@ data class AnswerStatistics(
     val totalCount: Int,
     val doctorsCount: Int,
     val nursesCount: Int,
-    val workersCount: Int
+    val workersCount: Int,
+    var HR: Int,
+    var HW: Int,
+    var Missed: Int
 ) {
     private val resources = MainApplication.instance.resources
 
-    fun getTotal(): String =
-        resources.getString(R.string.value_answer_total_count, answerText, totalCount)
-
-    fun getDoctorsPercentage(): String =
+    fun getHRPercentage(): String =
         resources.getString(
-            R.string.value_doctors_count,
-            doctorsCount,
-            String.format("%.2f", doctorsCount / totalCount.toFloat() * 100)
+            R.string.value_hr_percentage,
+            HR,
+            String.format("%.2f", HR / (HR + HW + Missed).toFloat() * 100)
         )
 
-    fun getNursesPercentage(): String =
+    fun getHWPercentage(): String =
         resources.getString(
-            R.string.value_nurses_count,
-            nursesCount,
-            String.format("%.2f", nursesCount / totalCount.toFloat() * 100)
+            R.string.value_hw_percentage,
+            HW,
+            String.format("%.2f", HW / (HR + HW + Missed).toFloat() * 100)
         )
 
-    fun getWorkersPercentage(): String =
+    fun getCombinedPercentage(): String =
         resources.getString(
-            R.string.value_workers_count,
-            workersCount,
-            String.format("%.2f", workersCount / totalCount.toFloat() * 100)
+            R.string.value_combined_percentage,
+            (HR + HW),
+            String.format("%.2f", (HR + HW) / (HR + HW + Missed).toFloat() * 100)
         )
-
 }
